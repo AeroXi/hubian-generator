89faaa41af142ffdf07c44f99ee55def891c6de3
AeroXi <31943024+AeroXi@users.noreply.github.com>
2020-03-13 15:07:18 +0800

    demo publish

--- main.js
+++ main.js
@@ -4,24 +4,29 @@ var app = new Vue({
       length: 10,
       event: "",
       author: "老胡",
+      paragraph: "",
     },
     methods: {
         sentence() {
             text = hu.text
-            return text[Math.floor(Math.random() * text.length)]
+            sentence = text[Math.floor(Math.random() * text.length)]
+            sentence = sentence.split("$author$").join(this.author)
+            sentence = sentence.split("$event$").join(this.event)
+            return sentence
         },
         generate(){
-            return
+            var array = []
+            for (i=0;i<this.length;i++){
+                var sentence = this.sentence()
+                array.push(sentence)
+            }
+            this.paragraph = array.join("")
+            return 
+            
         }
     },
     computed: {
     
-        paragraph() {
-            var array = []
-            for (i=0;i<this.length;i++){
-                array.push(this.sentence())
-            }
-            return array.join("")
-        }
+        
     }
   })
